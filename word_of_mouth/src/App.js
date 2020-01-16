5188f90a8f2ed8c7ac1d51fddcf8eca7f8426874
kendall motes <kmotes@reliaquest.com>
2020-01-15 22:26:25 -0500

    first sign up page

---
+++ word_of_mouth/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <header>Word of Mouth</header>
+        <br/>
+        <br/>
+        <br/>
+        <p>Sign Up</p>
+        <input placeholder={"Email"} />
+        <input placeholder={"Password"}/>
+
+        <br/>
+        <br/>
+        <input placeholder={"First Name"} />
+        <input placeholder={"Last Name"}/>
+
+        <br/>
+        <br/>
+        <input placeholder={"First Name"} />
+        <input placeholder={"Last Name"}/>
+
+        <br/>
+        <br/>
+        <input placeholder={"Age"} />
+        <input placeholder={"Gender"}/>
+
+        <br/>
+        <br/>
+        <button id={"signUp"}>Sign Up</button>
+
+    </div>
+  );
+}
+
+export default App;
